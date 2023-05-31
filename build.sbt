0b1f5520824358b5224193a503b4b25d005cbbbc
Roberto Morales <rockamorales@hotmail.com>
2023-05-31 12:04:29 -0600

    Project setup

--- build.sbt
+++ build.sbt
@@ -2,9 +2,9 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.10"
 
-lazy val akkaVersion = "2.6.10"
+lazy val akkaVersion = "2.8.2"
 lazy val scalaTestVersion = "3.1.4"
-lazy val akkaHttpVersion = "10.2.8"
+lazy val akkaHttpVersion = "10.5.2"
 
 lazy val root = (project in file("."))
   .settings(
@@ -14,7 +14,7 @@ lazy val root = (project in file("."))
 
       "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
       "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
-      "com.typesafe.akka" % "akka-http-testkit" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test,
 
       "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
       "org.scalatest" %% "scalatest" % scalaTestVersion
--- src/main/scala/playground/Playground.scala
+++ src/main/scala/playground/Playground.scala
@@ -1,5 +1,33 @@
 package playground
 
-object Playground extends App {
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives.{complete, pathEndOrSingleSlash}
 
+import scala.io.StdIn
+
+object Playground extends App {
+  implicit val system = ActorSystem("AkkaHttpPlayground")
+  import system.dispatcher
+
+  val simpleRoute =
+    pathEndOrSingleSlash {
+      complete(HttpEntity(
+        ContentTypes.`text/html(UTF-8)`,
+        """
+          |<html>
+          | <body>
+          |   Rock the JVM with Akka HTTP!
+          | </body>
+          |</html>
+          |""".stripMargin
+      ))
+    }
+
+  val bindingFuture = Http().newServerAt("localhost", 8000).bindFlow(simpleRoute)
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
 }
